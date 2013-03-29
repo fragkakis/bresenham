8b9195f607b70e3c1d3d3fd8effc6b4b46dc701c
Markos Fragkakis <markos.fragkakis@gmail.com>
2013-03-30 00:01:06 +0200

    Adding Bresenham algorithm and test

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 bresenham
 =========
 
-Bresenham's line algorithm
+This is an implementation of Bresenham's line algorithm.
+
+The implementation is based on the corresponding Wikipedia entry [1], specifically on the "Simplification" variation.
+
+The following is an example of a line from point (3,3) to point (17,7) on a 30x10 2d array.
+
++++++++++++++++++++++++++++++++++++++++++
++ | | | | | | | | | | | | | | | | | | | +
++ | | | | | | | | | | | | | | | | | | | +
++ | | | | | | | | | | | | | | | | | | | +
++ | | |X|X| | | | | | | | | | | | | | | +
++ | | | | |X|X|X|X| | | | | | | | | | | +
++ | | | | | | | | |X|X|X| | | | | | | | +
++ | | | | | | | | | | | |X|X|X|X| | | | +
++ | | | | | | | | | | | | | | | |X|X| | +
++ | | | | | | | | | | | | | | | | | | | +
++ | | | | | | | | | | | | | | | | | | | +
++++++++++++++++++++++++++++++++++++++++++
+
+
+[1] http://en.wikipedia.org/wiki/Bresenham's_line_algorithm
---
+++ src/test/java/org/fragkakis/BresenhamTest.java
@@ -0,0 +1,57 @@
+package org.fragkakis;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class BresenhamTest {
+
+	@Test
+	public void test() {
+		Point[][] grid = new Point[20][10];
+		for(int x=0; x<grid.length; x++) {
+			for(int y=0; y<grid[0].length; y++) {
+				grid[x][y] = new Point(x, y);
+			}
+		}
+		
+		List<Point> line = Bresenham.findLine(grid,3, 3, 17, 7);
+		
+		visualize(grid, line);
+	}
+
+	private void visualize(Point[][] grid, List<Point> line) {
+
+		StringBuffer sb = new StringBuffer();
+		// upper border
+		for(int x=0; x<grid.length*2 + 1; x++) {
+			 sb.append("+");
+        }
+        sb.append("\n");
+		
+        // content
+        
+        for(int y=0; y<grid[0].length; y++) {
+			
+			sb.append("+");
+			for(int x=0; x<grid.length; x++) {
+				if (x != 0) {
+                    sb.append("|");
+                }
+				sb.append(line.contains(grid[x][y]) ? "X" : " ");
+			}
+			sb.append("+");
+			sb.append("\n");
+		}
+		
+		// lower border
+		for(int x=0; x<grid.length*2 + 1; x++) {
+			sb.append("+");
+		}
+		sb.append("\n");
+		
+		System.out.println(sb.toString());
+	}
+	
+
+}
